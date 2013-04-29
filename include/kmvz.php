fb3440bf69791b5f57f03c0248207605b656c9a5
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-04-29 19:27:59 +0400

    kmvz: check for curl

--- include/kmvz.php
+++ include/kmvz.php
@@ -2,23 +2,24 @@
 
 
 	function kmvz_shorten($url) {
+		if (function_exists("curl_init")) {
+			$query = "http://kmvz.us/api.php?op=shorten&key=" .
+				urlencode(KMVZ_APIKEY) . "&url=" . urlencode($url);
 
-		$query = "http://kmvz.us/api.php?op=shorten&key=" .
-			urlencode(KMVZ_APIKEY) . "&url=" . urlencode($url);
+			$ch = curl_init($query);
 
-		$ch = curl_init($query);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+			curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$contents = @curl_exec($ch);
 
-		$contents = @curl_exec($ch);
-
-		if ($contents) {
-			$contents = json_decode($contents, true);
-			if ($contents && $contents['shorturl']) {
-				return $contents['shorturl'];
+			if ($contents) {
+				$contents = json_decode($contents, true);
+				if ($contents && $contents['shorturl']) {
+					return $contents['shorturl'];
+				}
 			}
 		}
 
