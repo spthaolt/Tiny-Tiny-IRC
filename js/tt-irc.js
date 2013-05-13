fbb9f1efa77d5dc9ff4f7b16d6434872ea0753f6
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-05-13 20:10:50 +0400

    reduce maximum log size

--- js/tt-irc.js
+++ js/tt-irc.js
@@ -1568,7 +1568,7 @@ function push_message(connection_id, channel, message, message_type, no_tab_hl) 
 
 			if (chan && chan.lines) {
 
-				while (chan.lines().length > 200)
+				while (chan.lines().length > 100)
 					chan.lines.shift();
 
 				chan.lines.push(new Message(message));
